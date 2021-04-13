1012a98bdc3602b5d03072a4391f4aca04d635db
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-13 16:46:21 +0530

    Renamed the input file to int-input

---
+++ programs/int-input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+  int number;
+  cout << "Enter a number" << endl;
+  cin >> number;
+  cout << "Number entered by you: " << number << endl;
+  return 0;
+}
