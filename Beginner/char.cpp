4435d3b7a6d48cdcf1bb29db31ba1cae3d743389
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-13 20:36:56 +0530

    Character data type
    Output the character and the numberical value of the character

---
+++ Beginner/char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  char x = 'T';
+  cout << "Character: " << x << endl;
+  cout << "Printing the numerical value of the character: " << (int)x << endl;
+  return 0;
+}
