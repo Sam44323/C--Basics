e3cb4523300ccb7281532d0562fa61e19d89b749
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-13 15:15:54 +0530

    Added some comments to declarative-demo.cpp file

--- programs/declaration-demo.cpp
+++ programs/declaration-demo.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-using std::cout; // using the declarative approach
+using std::cout; // using the declarative approach (will work for entire program)
 
 int main()
 {
+  // if declared here then going to work for this block only
+
   cout << "Using the declarative approach\n";
   return 0;
 }
