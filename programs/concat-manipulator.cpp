f30754113b3d71e63a762a00291cc245f66d1ace
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-13 15:28:47 +0530

    Added concat-manipulator for concepts of concatenation and manipulation tasks

---
+++ programs/concat-manipulator.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  int slices = 17;
+  std::cout << "You have " << slices << " slices of pizza" << std::endl; // endl servers the same purpose as \n
+  return 0;
+}
---
+++ programs/variables.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  int slices = 5;
+  std::cout << slices;
+  return 0;
+}
