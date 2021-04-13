1dd1dba028e60314cf9d47baf8140ae26466c0ee
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-13 17:44:36 +0530

    Added the power file for calculating the power of a number base on the input

---
+++ Intermediate/power.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void calulatePower(double base, int exp)
+{
+  cout << "Ouput for the required details is as followed " << pow(base, exp) << endl;
+}
+
+int main()
+{
+  double num1;
+  int exp;
+  cout << "Enter the number" << endl;
+  cin >> num1;
+  cout << "Enter the exponent" << endl;
+  cin >> exp;
+  calulatePower(num1, exp);
+  return 0;
+}
