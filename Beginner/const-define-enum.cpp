49805603596b945856c40dd3cd0d6b193b201130
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-15 13:39:38 +0530

    Added const,define and enum way for adding constants

---
+++ Beginner/const-define-enum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define X 10 // macro method for defining constants
+
+int main()
+{
+  const int i = x; // constants using const keyword
+  enum
+  {
+    y = 100
+  }; // constants using enum method
+  return 0;
+}
