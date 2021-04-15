7a0ad352f7810a986173973f2605c9178920757e
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-15 15:15:10 +0530

    Dec-hex and dec-oct conversion methods

---
+++ Intermediate/hex-oct.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x = 30;
+  cout << hex << x << endl; // printing the hex equiv of 30
+  cout << oct << x << endl; // printing the oct equiv of 30
+  return 0;
+}
