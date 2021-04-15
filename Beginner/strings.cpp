c37e211a28ed60d5585d01093f324883cb7a2eba
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-15 14:16:30 +0530

    strings file for string basic

---
+++ Beginner/strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string greetings;
+  std::cout << "Enter your greeting " << std::endl;
+  std::cin >> greetings;
+  std::cout << greetings << std::endl;
+  return 0;
+}
