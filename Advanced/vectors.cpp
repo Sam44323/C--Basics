c7da550aee6ad113b42dcba2d9a649dffd0ef8f8
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-24 02:54:44 +0530

    Refactoring

--- Advanced/vectors.cpp
+++ Advanced/vectors.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main()
 {
   vector<int> values = {1, 2, 3};
-  values.push_back(4);
+  values.push_back(13);
   cout << values[values.size() - 1] << endl;
+  values.pop_back();
+  cout << values.size() << endl;
   return 0;
 }
