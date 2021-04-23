8fc238a073ffdcb6285cc55efd4df63f66cba928
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-24 02:23:52 +0530

    Added the logic for creating a vector

--- Advanced/vectors.cpp
+++ Advanced/vectors.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+  vector<int> values = {1, 2, 3};
+  values.push_back(4);
+  cout << values[3] << endl;
   return 0;
 }
