65ce07db07a7b1ee222b674212e86cd70ac18cf4
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-24 13:13:10 +0530

    STL and vetors added
    Added STL arrays and vectors for printing using the range loop

--- Advanced/rangeLoop.cpp
+++ Advanced/rangeLoop.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <array>
 
 using namespace std;
 
 int main()
 {
   int data[5] = {1, 2, 3, 4, 5};
+  vector<int> values = {1, 2, 3, 4, 5};
+  array<int, 5> STL = {1, 2, 3, 4, 5};
 
   // range based loop (c++11 extension)
+  cout << "Arrays" << endl;
   for (int n : data)
+  {
+    cout << n << '\t';
+  }
+  cout << '\n';
+  cout << "Vectors" << endl;
+  for (int n : values)
+  {
+    cout << n << '\t';
+  }
+  cout << '\n';
+  cout << "STL Arrays" << endl;
+  for (int n : STL)
   {
     cout << n << '\t';
   }
